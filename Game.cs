83d10e50c9769bcdeb79e5700c0679638f4616d3
PorvaniVafo <parvanivafa.mokhammad@alatoo.edu.kg>
2025-03-21 18:42:56 +0600

    Some updates for Program.cs hehe

--- Game.cs
+++ Game.cs
@@ -30,3 +30,17 @@ namespace TicTacToe
         }
     }
 }
+public void DrawBoard()
+{
+    Console.Clear();
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            Console.Write(board[i, j]);
+            if (j < 2) Console.Write(" | ");
+        }
+        Console.WriteLine();
+        if (i < 2) Console.WriteLine("---------");
+    }
+}
